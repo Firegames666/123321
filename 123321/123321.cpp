0a85e31c5e3848e23137be99fdf03acb0ccbc92b
Никита <@DESKTOP-Q9SPLIR>
2022-10-17 19:37:30 +0300

    Добавьте файлы проекта.

---
+++ 123321/123321.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void type() {
+    char** arr = new char* [5];
+    for (int i = 0; i < 5; i++) {
+        *(arr + i) = new char[256];
+        cin >> *(arr + i);
+    }
+    int a = 0;
+    int b = 0;
+    for (int j = 0; j < 5; j++) {
+        if (b < strlen(*(arr + j))) {
+            b = strlen(*(arr + j));
+            a = j;
+        }
+    }
+    cout << *(arr + a);
+}
+
+int main()
+{
+    type();
+}
